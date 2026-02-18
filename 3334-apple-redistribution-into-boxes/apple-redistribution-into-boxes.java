6e7bd62049b4bd8d2dca46c774c56326cd0510a9
ARIVAZHAGAN B <barivazhagan7@gmail.com>
2026-02-18 14:14:01 +0530

    Time: 8 ms (5.15%) | Memory: 45 MB (6.03%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
@@ -3,14 +3,11 @@ class Solution {
         Arrays.sort(capacity);
         int sum = Arrays.stream(apple).sum();
         int index=0;
-        for(int i=0;i<capacity.length/2;i++){
-            int temp = capacity[i];
-            capacity[i] = capacity[capacity.length-i-1];
-            capacity[capacity.length-i-1] = temp;
-        }
+        int i=capacity.length-1;
         while(sum > 0){
-            sum-= capacity[index];
+            sum-= capacity[i];
             index++;
+            i--;
             if(sum <= 0) return index;
         }
         return index;
