859a6d2438731e8d03c5c4b13767af61fd44e668
ARIVAZHAGAN B <barivazhagan7@gmail.com>
2026-02-18 14:44:36 +0530

    Time: 10 ms (86.98%) | Memory: 47.6 MB (82.45%) - LeetSync

---
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countCollisions(String s) {
+        int n = s.length();
+        int left = 0;
+        int right = n - 1;
+
+        while (left < n && s.charAt(left) == 'L') left++;
+        while (right >= 0 && s.charAt(right) == 'R') right--;
+
+        int collisions = 0;
+
+        for (int i = left; i <= right; i++) {
+            if (s.charAt(i) != 'S') collisions++;
+        }
+
+        return collisions;
+    }
+}
