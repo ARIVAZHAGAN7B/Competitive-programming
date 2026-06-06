432b92fc5d25d292c4eb7ae12b91b50cad02edb0
ARIVAZHAGAN B <barivazhagan7@gmail.com>
2026-06-06 11:47:41 +0530

    Time: 28 ms (51.59%) | Memory: 46.2 MB (78.68%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -5,17 +5,7 @@ class Solution {
             int num1 = str.charAt(i-1) - '0';
             int num2 = str.charAt(i) - '0';
             int num3 = str.charAt(i+1) - '0';
-            if(num2 < num1 && num2 < num3) count++;
-        }
-        return count;
-    }
-    public int count_peak(String str){
-        int count = 0;
-        for(int i=1;i<str.length()-1;i++){
-            int num1 = str.charAt(i-1) - '0';
-            int num2 = str.charAt(i) - '0';
-            int num3 = str.charAt(i+1) - '0';
-            if(num2 > num1 && num2 > num3) count++;
+            if(num2 < num1 && num2 < num3 || num2 > num1 && num2 > num3) count++;
         }
         return count;
     }
@@ -24,7 +14,6 @@ class Solution {
         int count = 0;
         for(int i=num1;i<=num2;i++){
             String num = ""+i;
-            count += count_peak(num);
             count += count_valley(num);
         }
         return count;
